7ce7245199f101a93394f056a65ac4df7b7f9189
givemesomecoffee <givemesomecoffee@yandex.ru>
2022-10-16 14:46:54 +0300

    hw_done

--- feature/bfeatureimpl/src/main/java/com/example/b_feature_impl/BFeatureFragment.kt
+++ feature/bfeatureimpl/src/main/java/com/example/b_feature_impl/BFeatureFragment.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.b_feature_impl.di.BFeatureComponent
@@ -18,6 +20,9 @@ class BFeatureFragment : Fragment() {
 
     private val viewModel: BFeatureViewModel by viewModels()
 
+    private var loader: ProgressBar? = null
+    private var textField: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (BFeatureComponentHolder.get() as BFeatureComponent).inject(viewModel)
@@ -29,16 +34,18 @@ class BFeatureFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewModel.state.observe(viewLifecycleOwner, ::render)
-        return inflater.inflate(R.layout.b_feature_fragment, container, false)
+        return inflater.inflate(R.layout.b_feature_fragment, container, false).apply {
+            loader = findViewById(R.id.loading)
+            textField = findViewById(R.id.test_field)
+        }
     }
 
-    fun render(state: LceViewState<TestInteractorDto>) {
-        Log.d("custom", state.error.toString())
+    private fun render(state: LceViewState<TestInteractorDto>) {
+        loader?.isVisible = state.loading
         state.data?.let {
-            view?.findViewById<TextView>(R.id.test_field)?.text = it.master
+            textField?.text = it.master
         }
-        if (state.animateError) view?.findViewById<TextView>(R.id.test_field)?.text =
-            "Что-то пошло не так"
+        if (state.animateError) textField?.text = state.error
     }
 
     companion object {
